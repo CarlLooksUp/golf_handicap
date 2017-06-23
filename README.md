8589944addc80429689deb0b00d2ee4dc922d88f
Carl Morrissey <carl.morrissey@gmail.com>
2017-06-23 17:09:00 -0400

    working argument

---
+++ README.md
@@ -0,0 +1,14 @@
+##Requirements
+
+Test suite requires [pytest](http://docs.pytest.org/en/latest/) to run.
+
+    pip install pytest
+
+    py.test handicapper
+
+
+##Running
+
+Test input can be run through the test suite (handicapper/test/test_handicapper.py), or the handicapper can run independently. Call the file with the path to the rounds csv as an argument:
+
+    python handicapper/handicapper.py path/to/file.csv
--- handicapper/handicapper.py
+++ handicapper/handicapper.py
@@ -1,14 +1,12 @@
 import csv
+import sys
 from datetime import datetime
 from errors import *
 
 class Handicapper:
 
-    def __init__(self, rounds = [], filename = ""):
-        if len(rounds) > 0:
-            self.rounds = rounds
-            self.sort_rounds()
-        elif filename != "":
+    def __init__(self, filename = ""):
+        if filename != "":
             self.read_rows_file(filename)
 
     def read_rows_file(self, filename):
@@ -24,7 +22,6 @@ class Handicapper:
 
     def sort_rounds(self):
         self.rounds.sort(key = lambda x: datetime.strptime(x['DATE'], '%m/%d/%y'))
-        print self.rounds
 
     def differential(self, score, rating, slope):
         return ((float(score) - float(rating)) * 113 / float(slope))
@@ -51,3 +48,7 @@ class Handicapper:
         handicap = reduce(lambda holdover, _round:_round['DIFFERENTIAL'] + holdover, round_set, 0) / float(len(round_set)) * 0.96
 
         return round(handicap, 2)
+
+if __name__ == "__main__":
+    h = Handicapper(filename = sys.argv[1])
+    print h.handicap()
